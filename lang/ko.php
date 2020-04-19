c00e41f9f927b3f7758da93b81a22e56a81657da
xisoDev <xiso@xiso.co.kr>
2020-04-20 02:46:11 +0900

    한글 언어팩 추가

---
+++ lang/ko.php
@@ -0,0 +1,130 @@
+<?php
+
+return [
+    'auth' => [
+        'title' => '페이지빌더 매니저',
+        'username' => '사용자이름',
+        'password' => '비밀번호',
+        'login-button' => '로그인',
+        'invalid-credentials' => '사용자이름 또는 비밀번호가 잘못되었습니다.',
+    ],
+    'website-manager' => [
+        'title' => '페이지빌더 매니저',
+        'logout' => '로그아웃',
+        'pages' => '페이지',
+        'menus' => '메뉴',
+        'name' => '이름',
+        'page-title' => '페이지 타이틀',
+        'route' => '슬러그',
+        'layout' => '레이아웃',
+        'actions' => '동작',
+        'view' => '보기',
+        'edit' => '편집',
+        'remove' => '제거',
+        'add-new-page' => '새 페이지 추가',
+        'edit-page' => '페이지 편집',
+        'back' => '뒤로',
+        'save-changes' => '변경내용 저장',
+        'theme' => '테마',
+        'visible-in-page-overview' => '페이지개요에 표시',
+        'page-created' => '페이지가 성공적으로 생성 되었습니다.',
+        'page-updated' => '페이지가 성공적으로 업데이트 되었습니다.',
+        'page-deleted' => '페이지가 제거되었습니다.',
+
+        'settings' => '설정',
+        'website-languages' => '페이지 언어',
+        'languages-selector-placeholder' => '하나 이상의 언어를 선택하세요.',
+        'save-settings' => '설정 저장',
+        'settings-updated' => '설정이 성공적으로 업데이트 되었습니다.',
+        'pagebuilder-block-images' => 'Pagebuilder block thumbnails',
+        'render-thumbs' => '썸네일 렌더링',
+    ],
+    'pagebuilder' => [
+        'loading-text' => '페이지빌더 불러오는중...',
+        'style-no-element-selected' => '스타일을 수정할 요소를 선택하세요.',
+        'trait-no-element-selected' => '속성을 수정할 요소를 선택하세요.',
+        'trait-settings' => '속성설정',
+        'default-category' => '일반',
+        'view-blocks' => '블록 보기',
+        'view-settings' => '설정 보기',
+        'view-style-manager' => '스타일매니저',
+        'save-page' => '저장',
+        'view-page' => '보기',
+        'go-back' => '뒤로',
+        'page' => '페이지',
+        'page-content' => '페이지 내용',
+        'toastr-changes-saved' => '변경사항이 저장되었습니다.',
+        'toastr-saving-failed' => '저장중 에러가 발생하였습니다.',
+        'toastr-component-update-failed' => '컴포넌트를 호출하는 중 오류가 발생하였습니다.',
+        'toastr-switching-language-failed' => '언어를 변경하지 못했습니다.',
+        'yes' => '네',
+        'no' => '아니요',
+        'trait-manager' => [
+            'link' => [
+                'text' => '텍스트',
+                'target' => '새 창에서 열까요?'
+            ],
+            'no-settings' => '이 블록에는 설정이 없습니다.'
+        ],
+        'selector-manager' => [
+            'label' => 'CSS 클래스',
+            'states-label' => '대상 레이아웃',
+            'selected-label' => '선택됨',
+            'state-hover' => '마우스 오버시',
+            'state-active' => '클릭시',
+            'state-nth' => '홀/짝 요소',
+        ],
+        'style-manager' => [
+            'sectors' => [
+                'position' => '위치',
+                'background' => '배경',
+                'advanced' => '고급',
+            ],
+            'properties' => [
+                'position' => [
+                    'width' => '넓이',
+                    'min-width' => '최소 넓이',
+                    'max-width' => '최대 넓이',
+                    'height' => '높이',
+                    'min-height' => '최소 높이',
+                    'max-height' => '최대 높이',
+                    'padding' => [
+                        'name' => '안쪽여백',
+                        'properties' => [
+                            'padding-top' => '상단 안쪽여백',
+                            'padding-right' => '우측 안쪽여백',
+                            'padding-bottom' => '하단 안쪽여백',
+                            'padding-left' => '좌측 안쪽여백'
+                        ]
+                    ],
+                    'margin' => [
+                        'name' => '바깥여백',
+                        'properties' => [
+                            'margin-top' => '상단 바깥여백',
+                            'margin-right' => '우측 바깥여백',
+                            'margin-bottom' => '하단 바깥여백',
+                            'margin-left' => '좌측 바깥여백'
+                        ]
+                    ]
+                ],
+                'background' => [
+                    'background-color' => '배경색',
+                    'background' => '배경',
+                ]
+            ]
+        ],
+        'asset-manager' => [
+            'modal-title' => '이미지 선택',
+            'drop-files' => '이 곳으로 파일을 끌어다 놓거나 클릭하여 업로드합니다.',
+            'add-image' => '이미지 추가',
+        ],
+    ],
+    'languages' => [
+        'ko' => '한국어',
+        'en' => '영어',
+        'nl' => '네덜란드어',
+        'es' => '스페인어',
+        'fr' => '프랑스어',
+        'de' => '독이러',
+    ],
+];

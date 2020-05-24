4d0d2d37dfd8fa12cc061d996c2d49c7656f2a5f
xisoDev <xiso@xiso.co.kr>
2020-05-25 03:45:28 +0900

    db포트수정

--- config/config.example.php
+++ config/config.example.php
@@ -29,6 +29,7 @@ return [
         'database' => [
             'driver'    => 'mysql',
             'host'      => 'localhost',
+            'port'      => 3306,
             'database'  => '',
             'username'  => '',
             'password'  => '',
